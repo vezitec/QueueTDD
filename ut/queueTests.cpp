49be0e022f5643b9c93fe4e95b45206ded7b6153
Daniel Mazur <danielmazur333@gmail.com>
2022-07-07 11:15:16 +0200

    Envoirement preparation

---
+++ ut/queueTests.cpp
@@ -0,0 +1,15 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "queue.hpp"
+
+class QueueTests : public ::testing::Test
+{
+public:
+
+};
+
+TEST_F(QueueTests, Given_NewQueue_When_Initialization_Then_TwoPtrsAreNullptr)
+{
+
+}
+
